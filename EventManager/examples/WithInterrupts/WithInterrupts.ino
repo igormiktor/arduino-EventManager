11fdfc9db2afb0cb9ec32e872bcab72560480674
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2015-02-14 12:51:58 -0500

    Update example with interrupts to latest version of the library

--- EventManager/examples/WithInterrupts/WithInterrupts.ino
+++ EventManager/examples/WithInterrupts/WithInterrupts.ino
@@ -1,26 +1,26 @@
 
 /*
-  This sketch assumes an LED on pin 13 (built-in on Arduino Uno) and 
+  This sketch assumes an LED on pin 13 (built-in on Arduino Uno) and
   an LED on pin 8.  It blinks boths LED using timer interrupts.
-  
+
   Timer interrupts are generated using the MsTimer2 library available at
   http://playground.arduino.cc/Main/FlexiTimer2
 
   Author: igormt@alumni.caltech.edu
   Copyright (c) 2013 Igor Mikolic-Torreira
-  
+
   This software is free software; you can redistribute it
   and/or modify it under the terms of the GNU Lesser
   General Public License as published by the Free Software
   Foundation; either version 2.1 of the License, or (at
   your option) any later version.
- 
+
   This software is distributed in the hope that it will
   be useful, but WITHOUT ANY WARRANTY; without even the
   implied warranty of MERCHANTABILITY or FITNESS FOR A
   PARTICULAR PURPOSE.  See the GNU Lesser General Public
   License for more details.
- 
+
   You should have received a copy of the GNU Lesser
   General Public License along with this library; if not,
   write to the Free Software Foundation, Inc.,
@@ -46,20 +46,20 @@ EventManager gEM;
 
 
 
-// Our interrupt generates a toggle event for pin 13 (gPins[0]) every 
-// time called (set to be every 1s) and and a toggle event for 
-// pin 8 (gPins[1]) every third call (equivalent to every 3s)  
+// Our interrupt generates a toggle event for pin 13 (gPins[0]) every
+// time called (set to be every 1s) and and a toggle event for
+// pin 8 (gPins[1]) every third call (equivalent to every 3s)
 void interruptHandler()
 {
     static int oddEven = 0;
-    
+
     gEM.queueEvent( EventManager::kEventUser0, 0 );
-    
+
     if ( !oddEven )
     {
 	gEM.queueEvent( EventManager::kEventUser0, 1 );
     }
-    
+
     ++oddEven;
     oddEven %= 3;
 }
@@ -74,16 +74,18 @@ void listener( int event, int pin )
 }
 
 
+GenericCallable<void(int,int)> listenerFreeFunction(listener);
 
-void setup() 
-{                
+
+void setup()
+{
     // Setup
     Serial.begin( 115200 );
     pinMode( gPins[0].pinNbr, OUTPUT );
     pinMode( gPins[1].pinNbr, OUTPUT );
-    
+
     // Add our listener
-    gEM.addListener( EventManager::kEventUser0, listener );
+    gEM.addListener( EventManager::kEventUser0, &listenerFreeFunction );
 
     // Set up interrupts every second
     MsTimer2::set( 1000, interruptHandler ); // 1 sec period
@@ -91,7 +93,7 @@ void setup()
 }
 
 
-void loop() 
+void loop()
 {
     // Handle any events that are in the queue
     gEM.processEvent();
