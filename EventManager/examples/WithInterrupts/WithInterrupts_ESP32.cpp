59157f0b03baae11a7e7dd54eeb9f749561865e5
Igor Mikolic-Torreira <igormt@alumni.caltech.edu>
2020-09-09 18:49:07 -0400

    Create WithInterrupts_ESP32.cpp
    Example for ESP32

---
+++ EventManager/examples/WithInterrupts/WithInterrupts_ESP32.cpp
@@ -0,0 +1,69 @@
+#include <Arduino.h>
+
+#ifndef ESP32
+#error This code is designed to run on the ESP32 Arduino platform
+#endif
+
+// Using the GenericListeners branch of arduino-EventManager
+// https://github.com/igormiktor/arduino-EventManager/tree/GenericListeners
+#include <EventManager\EventManager.h>
+
+// Using the ESP32TimerInterrupt library
+// https://github.com/khoih-prog/ESP32TimerInterrupt
+#include <ESP32TimerInterrupt.h>
+
+EventManager eventManager;
+
+struct Pins
+{
+    int pinNumber;
+    int pinState;
+};
+
+Pins pins[2] = { { LED_BUILTIN, LOW }, { 4, LOW } };
+
+#define TIMER0_INTERVAL_MS 100UL
+ESP32Timer interruptTimer0(0);
+
+// IRAM_ATTR is required to place the handler in the right area of memory
+void IRAM_ATTR interruptHandler( void ) 
+{
+    static int oddEven = 0;
+
+    eventManager.queueEvent( EventManager::kEventUser0, 0 );
+
+    if ( !oddEven ) 
+    {
+        eventManager.queueEvent( EventManager::kEventUser0, 1 );
+    }
+
+    ++oddEven;
+    oddEven %= 3;
+}
+
+void listener( int event, int pin ) 
+{
+    pins[pin].pinState = pins[pin].pinState ? 0 : 1;
+    digitalWrite( pins[pin].pinNumber, pins[pin].pinState ? HIGH : LOW );
+}
+
+GenericCallable<void(int,int)> listenerFreeFunction( listener );
+
+void setup() 
+{
+    Serial.begin( 115200 );
+    Serial.println( "Entering setup()" );
+
+    pinMode( pins[0].pinNumber, OUTPUT );
+    pinMode( pins[1].pinNumber, OUTPUT );
+
+    eventManager.addListener( EventManager::kEventUser0, &listenerFreeFunction );
+
+    interruptTimer0.attachInterruptInterval( TIMER0_INTERVAL_MS * 1000UL, interruptHandler );
+}
+
+void loop() 
+{
+
+    eventManager.processEvent();
+}
